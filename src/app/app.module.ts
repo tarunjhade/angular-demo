d3bb3e5e94cb3fbec5e35eb826687a778f8c5e2b
Tarun Jhade <tarun@i-techsoftware.com>
2018-07-26 15:10:01 -0400

    Adding injectable service and third party service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.components';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
 
 
 @NgModule({
@@ -17,6 +19,10 @@ import { NavBarComponent } from './nav/navbar.component';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent
+  ],
+  providers: [
+    EventService,
+    ToastrService
   ],
   bootstrap: [EventsAppComponent]
 })
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,7 +8,7 @@ import {Component, Input} from '@angular/core';
         <div class="card-body">
             <h2>{{event?.name}}</h2>
             <div>Date: {{event?.date}}</div>
-            <div [ngClass]="{'text-success': event?.time === '8:00 am','text-danger': event?.time === '10:00 am'}" [ngSwitch]="event?.time">
+            <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
                 Time: {{event?.time}}
                 <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
                 <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -23,10 +23,18 @@ import {Component, Input} from '@angular/core';
             <div *ngIf="event?.onlineUrl">Online URL: {{events?.onlineUrl}}</div>
         </div>
     </div>`,
-    styles:[`
+    styles: [ `
     .thumbnail{ min-height:210px;}
-    `]
+    ` ]
 })
 export class EventThumbnailComponent {
     @Input() event: any;
+    getStartTimeClass() {
+        if (this.event && this.event.time === '8:00 am') {
+            return ['text-success'];
+        } else if (this.event && this.event.time === '10:00 am') {
+            return ['text-danger'];
+        }
+        return [];
+    }
 }
