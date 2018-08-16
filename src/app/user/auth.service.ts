4370185976991932b56ebb28c30d51b1ddb3bd6b
Tarun Jhade <tarun@i-techsoftware.com>
2018-08-16 16:47:54 -0400

    Integrating authentication with http/rxjs

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { IUser } from './user.model';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable()
 export class AuthService {
     currentUser: IUser;
+
+    constructor(private http: HttpClient) { }
     loginUser(userName: string, password: string) {
-        this.currentUser = {
-            id: 1,
-            firstName: 'john',
-            lastName: 'papa',
-            userName: userName
-        };
+        const loginInfo = { username: userName, password: password };
+        const options = { headers: new HttpHeaders({ 'content-type': 'application/json' }) };
+        const url = '/api/login';
+        return this.http.post(url, loginInfo, options)
+            .pipe(tap(data => {
+                this.currentUser = <IUser>data['user'];
+            }))
+            .pipe(catchError(err => {
+                return of(false);
+            }));
     }
 
     isAuthenticated() {
